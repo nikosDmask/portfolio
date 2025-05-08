10981d8c42cdc052eb5fe1bc28f41ed91f0a30fc
nikosDmask <nikosdevmask@gmail.com>
2025-05-08 17:11:03 +0300

    chore: remove ignored files from repository

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -84,7 +84,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-  'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+    )
 }
 
 
